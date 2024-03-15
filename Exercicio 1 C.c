9c31860b4f531695cf45e2929776ad41ffa5cda8
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-03-15 16:41:11 -0300

    Add files via upload

---
+++ Exercicio 1 C.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(){
+    
+    int primeiro_numero, segundo_numero, soma, subtracao, multiplicacao, divisao;
+
+    printf("Digite o Primeiro numero: ");
+    scanf("%i", &primeiro_numero);
+
+    printf("Digite o Segundo numero: ");
+    scanf("%i", &segundo_numero);
+
+    fflush(stdin);
+
+    soma = primeiro_numero + segundo_numero;
+    subtracao = primeiro_numero - segundo_numero;
+    multiplicacao = primeiro_numero * segundo_numero;
+    divisao = primeiro_numero / segundo_numero;
+    fflush(stdin);
+    
+    printf("=== Exibindo Resultados ===\n");
+    printf("A soma: %i \n", soma);
+    printf("A subtracao: %i\n", subtracao);
+    printf("A multiplicacao: %i\n", multiplicacao);
+    printf("A Divisao: %i\n", divisao);
+   
+    return  0;
+}

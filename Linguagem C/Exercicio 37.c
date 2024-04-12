f1c78ebea33efce60415e93cac7e7c87f6aa88b1
adrianomts7 <161032614+adrianomts7@users.noreply.github.com>
2024-04-12 10:28:18 -0300

    Update Exercicio 37.c

--- Linguagem C/Exercicio 37.c
+++ Linguagem C/Exercicio 37.c
@@ -10,23 +10,23 @@ int main(){
 	int i,numerosNegativos = 0;
 	
 	for(i = 0; i < MAX; i++){
-		printf("Digite o %iº Número: ", i + 1);
+		printf("Digite o %iÂº NÃºmero: ", i + 1);
 		scanf("%f", &numeros[i]);
 		fflush(stdin);
 		
 		if(numeros[i] < 0){
 			numerosNegativos ++;
 		}
-		else{
+		else if(numeros[i] > 0){
 			soma += numeros[i];
 		}
 	}
 	
 	for(i = 0; i < MAX; i++){
-		printf("O %iº Número digitado: %.1f\n",i + 1,numeros[i]);
+		printf("O %iÂº NÃºmero digitado: %.1f\n",i + 1,numeros[i]);
 	}
 	
-	printf("Foram digitados %i Número(s) Negativos\n",numerosNegativos);
-	printf("A Soma dos Números Positivos: %.1f\n", soma);
+	printf("Foram digitados %i NÃºmero(s) Negativos\n",numerosNegativos);
+	printf("A Soma dos NÃºmeros Positivos: %.1f\n", soma);
 	return 0;
 }
